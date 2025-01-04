9df9e99b707ea8cb1b5f223596f61138b0cf52b3
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-01-04 16:34:00 -0300

    Refactor Usuario model and remove UsuarioRepository interface

--- spring/src/main/java/dev/wallace/spring/model/Usuario.java
+++ spring/src/main/java/dev/wallace/spring/model/Usuario.java
@@ -1,11 +1,43 @@
 package dev.wallace.spring.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Usuario {
     @Id
-    @GeneratedValue(strategy=Geration)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable=false, unique=true) 
+    private String email;
+
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    @Column(nullable=false)
+    private String senha;
+
+
+    
 
 }
--- spring/src/main/java/dev/wallace/spring/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.wallace.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, longo>{
-
-}
