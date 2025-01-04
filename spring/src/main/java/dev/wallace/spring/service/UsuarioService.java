03793764f2486dd095612608774d3cbf30be26e1
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-01-04 16:58:14 -0300

    Add UsuarioService class for user management functionality

---
+++ spring/src/main/java/dev/wallace/spring/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package dev.wallace.spring.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import dev.wallace.spring.dto.UsuarioDTO;
+import dev.wallace.spring.model.Usuario;
+import dev.wallace.spring.repository.UsuarioRepository;
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+        
+    }
+
+
+    public List<UsuarioDTO> listar(){
+        return usuarioRepository.findAll().stream()
+        .map(usuario -> new UsuarioDTO(usuario.getId(), usuario.getEmail()))
+        .collect(Collectors.toList());
+    }
+
+ 
+
+
+}
